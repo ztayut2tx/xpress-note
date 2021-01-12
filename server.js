f9082fe2c781ca8bd22983afb9a4f64d0c0d8290
Zach Taylor <ztayut2tx@gmail.com>
2021-01-12 15:44:34 -0600

    application node modules and routes set up and linked between all pages

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const express = require("express");
+const fs = require ("fs");
+
+
+var app = express();
+var PORT = 3000;
+
+var apiRoute = require("./routes/apiroute.js");
+var htmlroute = require("./routes/htmlroute.js");
+
+app.use(express.urlencoded({ extended: true, }));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(apiRoute);
+app.use(htmlroute);
+
+app.listen(PORT, function() {
+    console.log(`Application listening on PORT : ${PORT}`)
+});
+
+
+
+
